c51d4a3929925e0c6946bb4c06002040c71904c4
cb1cyf <41017545+cb1cyf@users.noreply.github.com>
2022-05-05 23:52:56 +0800

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -1,4 +1,4 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, Row
 from pyspark.sql.types import *
 from pyspark.ml.recommendation import ALS
 from pyspark.ml.evaluation import RegressionEvaluator
@@ -19,21 +19,21 @@ def InsertUser(userName, password):
     }
     userDF = spark.read.jdbc('jdbc:mysql://mysql:3306/recommend', 'user', properties=prop)
     userDF.createOrReplaceTempView('user')
-    sql = f"select * from user where userName='{userName}'"
+    sql = "select * from user where userName='{}'".format(userName)
     sqlRes = spark.sql(sql)
     if sqlRes.count():
         print('[ERROR] Existing User Name')
         spark.stop()
         return False
-    inputRDD = spark.sparkContext.parallelize([f'{userName} {password}']).map(lambda line : line.split(' '))
+    inputRDD = spark.sparkContext.parallelize(['{} {}'.format(userName, password)]).map(lambda line: line.split(' '))
     schema = StructType([
         StructField('userName', StringType(), True),
         StructField('password', StringType(), True)
     ])
-    rowRDD = inputRDD.map(lambda p : Row(p[0].strip(), p[1].strip()))
+    rowRDD = inputRDD.map(lambda p: Row(p[0].strip(), p[1].strip()))
     rowDF = spark.createDataFrame(rowRDD, schema)
     rowDF.write.jdbc('jdbc:mysql://mysql:3306/recommend', 'user', 'append', prop)
-    print(f'[INFO] Insert ({userName}, {password}) into Table user')
+    print('[INFO] Insert ({}, {}) into Table user'.format(userName, password))
     spark.stop()
     return True
 
@@ -52,8 +52,9 @@ def Delete(sql):
     cursor.close()
     conn.close()
 
-# store result of recommendation
-def InsertRecommend(userId, bookId, rating):
+# store recommendation
+def InsertRecommend(userId, ratingList):
+    # ratingList = [(bookId, rating)]
     spark = SparkSession.builder.master('spark://master:7077').appName('InsertRecommend').getOrCreate()
     prop = {
         'user': 'root',
@@ -63,21 +64,23 @@ def InsertRecommend(userId, bookId, rating):
     }
     recommendDF = spark.read.jdbc('jdbc:mysql://mysql:3306/recommend', 'recommend', properties=prop)
     recommendDF.createOrReplaceTempView('recommend')
-    sql = f'select * from recommend where userId={userId}'
+    sql = 'select * from recommend where userId={}'.format(userId)
     sqlRes = spark.sql(sql)
     if sqlRes.count():
-        deleteSQL = f'delete from recommend where userId={userId}'
+        deleteSQL = 'delete from recommend where userId={}'.format(userId)
         Delete(deleteSQL)
-    inputRDD = spark.sparkContext.parallelize([f'{userId} {bookId} {rating}']).map(lambda line : line.split(' '))
+        print('[INFO] Delete old recommendation for userId={}'.format(userId))
+    inputData = ['{} {} {}'.format(userId, term[0], term[1]) for term in ratingList]
+    inputRDD = spark.sparkContext.parallelize(inputData).map(lambda line: line.split(' '))
     schema = StructType([
         StructField('userId', IntegerType(), True),
         StructField('bookId', IntegerType(), True),
         StructField('rating', FloatType(), True)
     ])
-    rowRDD = inputRDD.map(lambda p : Row(int(p[0]), int(p[1]), float(p[2])))
+    rowRDD = inputRDD.map(lambda p: Row(int(p[0]), int(p[1]), float(p[2])))
     rowDF = spark.createDataFrame(rowRDD, schema)
     rowDF.write.jdbc('jdbc:mysql://mysql:3306/recommend', 'recommend', 'append', prop)
-    print(f'[INFO] Insert ({userId}, {bookId}, {rating}) into Table recommend')
+    print('[INFO] Insert {} recommendation for userId={}'.format(len(ratingList), userId))
     spark.stop()
 
 # login and verify
@@ -91,19 +94,19 @@ def ReadUser(userName, password):
     }
     userDF = spark.read.jdbc('jdbc:mysql://mysql:3306/recommend', 'user', properties=prop)
     userDF.createOrReplaceTempView('user')
-    sql = f"select * from user where userName='{userName}'"
+    sql = "select * from user where userName='{}'".format(userName)
     sqlRes = spark.sql(sql)
     if not sqlRes.count():
         print('[ERROR] Wrong User Name')
         spark.stop()
-        return None
+        return -1
     res = sqlRes.collect()[0]
     if res['password'] != password:
         print('[ERROR] Wrong Password')
         spark.stop()
-        return None
+        return -2
     userId = res['userId']
-    print(f'[INFO] User {userName} Login with Password {password} and ID {userId}')
+    print('[INFO] User {} login with password {} and ID {}'.format(userName, password, userId))
     spark.stop()
     return userId
 
@@ -118,10 +121,10 @@ def ReadRecommend(userId):
     }
     recommendDF = spark.read.jdbc('jdbc:mysql://mysql:3306/recommend', 'recommend', properties=prop)
     recommendDF.createOrReplaceTempView('recommend')
-    sql = f'select bookId, rating from recommend where userId={userId}'
+    sql = 'select bookId, rating from recommend where userId={}'.format(userId)
     sqlRes = spark.sql(sql).collect()
     if not len(sqlRes):
-        print('[WARN] None Recommend Result')
+        print('[WARN] None Recommendation')
         spark.stop()
         return None
     bookDF = spark.read.jdbc('jdbc:mysql://mysql:3306/recommend', 'book', properties=prop)
@@ -130,7 +133,7 @@ def ReadRecommend(userId):
     for i in range(len(sqlRes)):
         bookId = sqlRes[i]['bookId']
         rating = sqlRes[i]['rating']
-        bookSQL = f'select * from book where bookId={bookId}'
+        bookSQL = 'select * from book where bookId={}'.format(bookId)
         bookRes = spark.sql(bookSQL).collect()
         if not len(bookRes):
             continue
@@ -139,7 +142,47 @@ def ReadRecommend(userId):
         author = bookRes[0]['author']
         url = bookRes[0]['url']
         res.append(tuple(bookId, isbn, title, author, url, rating))
+    print('[INFO] Read {} recommendation for userId={}'.format(len(res), userId))
+    spark.stop()
     return res
+
+"""
+Score, Recommend
+"""
+# store user score in CSV
+def Score(userId, scoreList):
+    # scoreList: [(bookId, score)]
+    spark = SparkSession.builder.master('spark://master:7077').appName('Score').getOrCreate()
+    scoreData = ['{} {} {}'.format(userId, term[0], term[1]) for term in scoreList]
+    scoreRDD = spark.sparkContext.parallelize(scoreData).map(lambda line: line.split(' '))
+    schema = StructType([
+        StructField('userId', IntegerType(), True),
+        StructField('bookId', IntegerType(), True),
+        StructField('rating', FloatType(), True)
+    ])
+    rowRDD = scoreRDD.map(lambda p: Row(int(p[0]), int(p[1]), float(p[2])))
+    rowDF = spark.createDataFrame(rowRDD, schema)
+    rowDF.write.csv('hdfs://namenode:8020/input/UserRatings/', mode='append', sep=';')
+    print('[INFO] Store {} score for userId={}'.format(len(scoreList), userId))
+    spark.stop()
+
+def line2row(line):
+    line = line.split(';')
+    row = {}
+    row['userId'] = int(line[0].strip('"'))
+    row['bookId'] = int(line[1].strip('"'))
+    row['rating'] = float(line[2].strip('"'))
+    return row
+
+# recommend with ALS; whether training is decided by flag (train after new score)
+def recommend(userId, flag):
+    spark = SparkSession.builder.master('spark://master:7077').appName('ALS').getOrCreate()
+    sc = spark.sparkContext
+    Ratings = sc.textFile('hdfs://namenode:8020/input/Ratings.csv').zipWithIndex().filter(lambda x: x[1]>0).map(lambda line: Row(**line2row(line[0]))).toDF()
+    UserRatings = sc.textFile('hdfs://namenode:8020/input/UserRatings').map(lambda line: Row(**line2row(line))).toDF()
+    Ratings = Ratings.union(UserRatings)
+    train, test = Ratings.randomSplit([0.8, 0.2])
+    pass
 
 if __name__ == '__main__':
     assert len(sys.argv) >= 2
@@ -147,11 +190,14 @@ if __name__ == '__main__':
         assert len(sys.argv) == 4
         flag = InsertUser(sys.argv[2], sys.argv[3])
     elif sys.argv[1] == 'InsertRecommend':
-        assert len(sys.argv) == 5
-        InsertRecommend(sys.argv[2], sys.argv[3], sys.argv[4])
+        assert len(sys.argv) == 4
+        InsertRecommend(sys.argv[2], sys.argv[3])
     elif sys.argv[1] == 'ReadUser':
         assert len(sys.argv) == 4
         userId = ReadUser(sys.argv[2], sys.argv[3])
     elif sys.argv[1] == 'ReadRecommend':
         assert len(sys.argv) == 3
         recommendResult = ReadRecommend(sys.argv[2])
+    elif sys.argv[1] == 'Score':
+        assert len(sys.argv) == 4
+        Score(sys.argv[2], sys.argv[3])
