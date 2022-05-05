1385b1963eb61735dd7e9e139733dc68dabcf5a6
cb1cyf <41017545+cb1cyf@users.noreply.github.com>
2022-05-05 22:10:18 +0800

    Add files via upload

--- start.sh
+++ start.sh
@@ -30,6 +30,7 @@ else
     cp ./CSV/Ratings.csv ./input_files/
     docker exec namenode hdfs dfs -mkdir /input
     docker exec namenode hdfs dfs -put /input_files/Ratings.csv /input
+    docker exec namenode hdfs dfs -mkdir /input/UserRatings/
     echo '[INFO] File Submitted'
 
     echo '[INFO] Copying Code & Driver to Spark Master...'
