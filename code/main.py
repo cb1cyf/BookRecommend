5e506790ac821447769a5d62a82f785f75c6d6b4
cb1cyf <41017545+cb1cyf@users.noreply.github.com>
2022-05-06 21:35:32 +0800

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -3,7 +3,7 @@ from pyspark.sql.functions import lit
 from pyspark.sql.types import *
 from pyspark.ml.recommendation import ALS, ALSModel
 from pyspark.ml.evaluation import RegressionEvaluator
-from pyspark.ml.tuning import CrossValidator, ParamGridBuilder
+from pyspark.ml.tuning import CrossValidator, ParamGridBuilder, CrossValidatorModel
 import sys
 import pymysql
 
@@ -54,9 +54,21 @@ def Delete(sql):
     cursor.close()
     conn.close()
 
+# parse list arg
+def parser(strList):
+    # strList: [[bookId, rating]]; type: str
+    strList = strList.split(',')
+    l = len(strList)
+    res = []
+    for i in range(0, l, 2):
+        bookId = int(strList[i].strip('[').strip(']'))
+        rating = float(strList[i+1].strip('[').strip(']'))
+        res.append((bookId, rating))
+    return res
+
 # store recommendation
 def InsertRecommend(userId, ratingList):
-    # ratingList = [(bookId, rating)]
+    # ratingList = [[bookId, rating]]; typ: str
     spark = SparkSession.builder.master('spark://master:7077').appName('InsertRecommend').getOrCreate()
     prop = {
         'user': 'root',
@@ -72,6 +84,7 @@ def InsertRecommend(userId, ratingList):
         deleteSQL = 'delete from recommend where userId={}'.format(userId)
         Delete(deleteSQL)
         print('[INFO] Delete old recommendation for userId={}'.format(userId))
+    ratingList = parser(ratingList)
     inputData = ['{} {} {}'.format(userId, term[0], term[1]) for term in ratingList]
     inputRDD = spark.sparkContext.parallelize(inputData).map(lambda line: line.split(' '))
     schema = StructType([
@@ -131,32 +144,16 @@ def ReadRecommend(userId):
         return None
     bookDF = spark.read.jdbc('jdbc:mysql://mysql:3306/recommend', 'book', properties=prop)
     resDF = sqlRes.join(bookDF, 'bookId', 'inner')
+    """
     res = []
     def f(row):
-        res.append(tuple(row['bookId'], row['ISBN'], row['title'], row['author'], row['url'], row['rating']))
+        res.append((row['bookId'], row['ISBN'], row['title'], row['author'], row['url'], row['rating']))
     resDF.foreach(f)
     """
-    sqlRes = spark.sql(sql).collect()
-    if not len(sqlRes):
-        print('[WARN] None Recommendation')
-        spark.stop()
-        return None
-    bookDF = spark.read.jdbc('jdbc:mysql://mysql:3306/recommend', 'book', properties=prop)
-    bookDF.createOrReplaceTempView('book')
+    resRDD = resDF.collect()
     res = []
-    for i in range(len(sqlRes)):
-        bookId = sqlRes[i]['bookId']
-        rating = sqlRes[i]['rating']
-        bookSQL = 'select * from book where bookId={}'.format(bookId)
-        bookRes = spark.sql(bookSQL).collect()
-        if not len(bookRes):
-            continue
-        isbn = bookRes[0]['ISBN']
-        title = bookRes[0]['title']
-        author = bookRes[0]['author']
-        url = bookRes[0]['url']
-        res.append(tuple(bookId, isbn, title, author, url, rating))
-    """
+    for row in resRDD:
+        res.append([row['bookId'], row['ISBN'], row['title'], row['author'], row['url'], row['rating']])
     print('[INFO] Read {} recommendation for userId={}'.format(len(res), userId))
     spark.stop()
     return res
@@ -166,8 +163,9 @@ Score, Recommend
 """
 # store user score in CSV
 def Score(userId, scoreList):
-    # scoreList: [(bookId, score)]
+    # scoreList: [[bookId, score]]; type: str
     spark = SparkSession.builder.master('spark://master:7077').appName('Score').getOrCreate()
+    scoreList = parser(scoreList)
     scoreData = ['{} {} {}'.format(userId, term[0], term[1]) for term in scoreList]
     scoreRDD = spark.sparkContext.parallelize(scoreData).map(lambda line: line.split(' '))
     schema = StructType([
@@ -177,7 +175,7 @@ def Score(userId, scoreList):
     ])
     rowRDD = scoreRDD.map(lambda p: Row(int(p[0]), int(p[1]), float(p[2])))
     rowDF = spark.createDataFrame(rowRDD, schema)
-    rowDF.write.csv('hdfs://namenode:8020/input/UserRatings/', mode='append', sep=';')
+    rowDF.write.csv('hdfs://namenode:8020/input/UserRatings', mode='append', sep=';')
     print('[INFO] Store {} score for userId={}'.format(len(scoreList), userId))
     spark.stop()
 
@@ -191,21 +189,30 @@ def line2row(line):
 
 # recommend with ALS; whether training is decided by flag (train after new score)
 def Recommend(userId, flag):
+    # flag: 1 = train; 0 = not train
     spark = SparkSession.builder.master('spark://master:7077').appName('ALS').getOrCreate()
     sc = spark.sparkContext
     UserRatings = sc.textFile('hdfs://namenode:8020/input/UserRatings').map(lambda line: Row(**line2row(line))).toDF()
-    if flag:
+    if flag == '1':
         Ratings = sc.textFile('hdfs://namenode:8020/input/Ratings.csv').zipWithIndex().filter(lambda x: x[1]>0).map(lambda line: Row(**line2row(line[0]))).toDF()
         dataset = Ratings.union(UserRatings)
+        """
         als = ALS(userCol='userId', itemCol='bookId', ratingCol='rating')
-        paramGrid = ParamGridBuilder().addGrid(als.rank, list(range(10, 21, 2))).addGrid(als.maxIter, [10, 20, 30]).addGrid(als.regParam, list(range(0.1, 1.1, 0.1))).build()
-        evaluator = RegressionEvaluator('prediction', 'rating', 'rmse')
-        cv = CrossValidator(als, paramGrid, evaluator)
+        paramGrid = ParamGridBuilder().addGrid(als.rank, list(range(10, 21, 2))).addGrid(als.maxIter, [10, 20, 30]).addGrid(als.regParam, [i/10.0 for i in range(1, 11)]).build()
+        evaluator = RegressionEvaluator(predictionCol='prediction', labelCol='rating', metricName='rmse')
+        cv = CrossValidator(estimator=als, estimatorParamMaps=paramGrid, evaluator=evaluator)
         cvALS = cv.fit(dataset)
         bestALS = cvALS.bestModel
         bestALS.write().overwrite().save('/usr/model')
+        """
+        als = ALS(rank=10, maxIter=10, regParam=0.1, userCol='userId', itemCol='bookId', ratingCol='rating')
+        model = als.fit(dataset)
+        # ISSUE: save on HDFS instead of local path like '/usr/model'
+        # https://stackoverflow.com/questions/41881191/empty-collection-error-when-trying-to-load-a-saved-spark-model-using-pyspark
+        # https://stackoverflow.com/questions/40327379/why-cant-i-load-a-pyspark-randomforestclassifier-model
+        model.write().overwrite().save('hdfs://namenode:8020/input/model')
     else:
-        cvALS = ALSModel.load('/usr/model')
+        model = ALSModel.load('hdfs://namenode:8020/input/model')
     UserRatings.createOrReplaceTempView('UserRatings')
     sql = 'select bookId from UserRatings where userId={}'.format(userId)
     userBook = spark.sql(sql)
@@ -215,18 +222,35 @@ def Recommend(userId, flag):
         'driver': 'com.mysql.jdbc.Driver',
         'useSSL': 'false'
     }
-    bookDF = spark.read.jdbc('jdbc:mysql://mysql:3306/recommend', 'book', properties=prop)
-    candidate = bookDF.filter(not bookDF.bookId.isin(userBook.bookId)).bookId
-    candidate = candidate.withColumn('userId', lit(userId)).withColumn('rating', lit(0.0))
-    prediction = cvALS.transform(candidate).select('userId', 'bookId', 'prediction')
-    resDF = prediction.sort('prediction', ascending=False).take(10)
+    bookDF = spark.read.jdbc('jdbc:mysql://mysql:3306/recommend', 'book', properties=prop).select('bookId')
+    candidate = bookDF.subtract(userBook)
+    candidate = candidate.withColumn('userId', lit(int(userId))).withColumn('rating', lit(0.0))
+    prediction = model.transform(candidate).select('bookId', 'prediction').fillna(-1).filter('prediction>0')
+    resRowList = prediction.sort('prediction', ascending=False).take(10)
+    """
     res = []
     def f(row):
-        res.append(tuple(row['bookId'], row['prediction']))
+        res.append((row['bookId'], row['prediction']))
     resDF.foreach(f)
-    print('[INFO] 10 recommendation by ALS for userId={}'.format(userId))
+    """
+    res = []
+    for row in resRowList:
+        res.append([row['bookId'], row['prediction']])
+    print('[INFO] {} recommendation by ALS for userId={}'.format(len(res), userId))
     spark.stop()
     return res
+
+
+"""
+Command:
+docker exec master \
+/usr/spark-2.3.0/bin/spark-submit \
+--jars /usr/spark-2.3.0/jars/mysql-connector-java-5.1.40/mysql-connector-java-5.1.40-bin.jar \
+--driver-class-path /usr/spark-2.3.0/jars/mysql-connector-java-5.1.40/mysql-connector-java-5.1.40-bin.jar \
+/usr/submit/main.py \
+func func_arg1 func_arg2
+"""
+
 
 if __name__ == '__main__':
     assert len(sys.argv) >= 2
@@ -247,4 +271,5 @@ if __name__ == '__main__':
         Score(sys.argv[2], sys.argv[3])
     elif sys.argv[1] == 'Recommend':
         assert len(sys.argv) == 4
+        assert sys.argv[3] in ['0', '1']
         res = Recommend(sys.argv[2], sys.argv[3])
